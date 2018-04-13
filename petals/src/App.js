a64b052c9b4ab154ca6f86e2e6dc46eae3133f92
taxio <taxio.2029@gmail.com>
2018-04-13 22:35:32 +0900

    install react-router-dom but not use now

--- petals/src/App.js
+++ petals/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -8,11 +9,13 @@ class App extends Component {
           <div className="navbar navbar-default navbar-fixed-top">
             <div className="container">
               <div className="navbar-header">
-                <a href="/" className="navbar-brand">Saffron</a>
+                {/*<Link to="#" className="navbar-brand">Saffron</Link>*/}
+                <a href="#" className="navbar-brand">Saffron</a>
               </div>
               <div className="navbar-collapse collapse" id="navbar-main">
                 <div className="nav navbar-form navbar-right">
                   <div className="form-group">
+                    {/*<Link to="#" className="btn btn-primary">Sign up</Link>*/}
                     <a href="#" className="btn btn-primary">Sign up</a>
                   </div>
                 </div>
