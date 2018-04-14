8a3c1770ee5895ccb9d9d9310fa1c7a057db3c1b
taxio <taxio.2029@gmail.com>
2018-04-14 14:25:12 +0900

    routing

--- petals/src/App.js
+++ petals/src/App.js
@@ -1,32 +1,17 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Route, Switch } from 'react-router';
+import { HashRouter } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <header>
-          <div className="navbar navbar-default navbar-fixed-top">
-            <div className="container">
-              <div className="navbar-header">
-                {/*<Link to="#" className="navbar-brand">Saffron</Link>*/}
-                <a href="#" className="navbar-brand">Saffron</a>
-              </div>
-              <div className="navbar-collapse collapse" id="navbar-main">
-                <div className="nav navbar-form navbar-right">
-                  <div className="form-group">
-                    {/*<Link to="#" className="btn btn-primary">Sign up</Link>*/}
-                    <a href="#" className="btn btn-primary">Sign up</a>
-                  </div>
-                </div>
+import TopPage from './components/TopPage';
 
-              </div>
-            </div>
-          </div>
-        </header>
-      </div>
-    );
-  }
-}
+export const App = () => (
+  <HashRouter>
+    <Route path="/" component={ MainRoute } />
+  </HashRouter>
+);
 
-export default App;
+const MainRoute = () => (
+  <Switch>
+    <Route path="/" component={ TopPage }/>
+  </Switch>
+);
