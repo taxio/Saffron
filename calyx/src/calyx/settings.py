033f3d29457e3ffa3d80b171c331e951e3480fb6
Pudding <s.kokuryo@gmail.com>
2018-04-17 00:39:05 +0900

    fix env key mistakes

--- calyx/src/calyx/settings.py
+++ calyx/src/calyx/settings.py
@@ -75,13 +75,15 @@ DATABASES = {
 
 EMAIL_HOST = os.getenv('CALYX_EMAIL_HOST')
 EMAIL_PORT = int(os.getenv('CALYX_EMAIL_PORT'))
-EMAIL_HOST_USER = os.getenv('CALYX_EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.getenv('CALYX_EMAIL_PASSWORD')
+EMAIL_HOST_USER = os.getenv('CALYX_EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('CALYX_EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = os.getenv('CALYX_EMAIL_USE_TLS', 'False').lower() == 'true'
 DEFAULT_FROM_EMAIL = os.getenv('CALYX_EMAIL_DEFAULT_FROM')
 
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # デバッグ時はコンソールに出力
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 DJOSER = {
     'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
