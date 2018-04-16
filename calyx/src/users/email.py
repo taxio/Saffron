d144ce9309c073ca3fec7dc7b309ae3e952a45ba
Pudding <s.kokuryo@gmail.com>
2018-04-17 01:11:45 +0900

    add confirmation email class

--- calyx/src/users/email.py
+++ calyx/src/users/email.py
@@ -1,5 +1,18 @@
 from django.conf import settings
-from djoser.email import ActivationEmail
+from djoser.email import ActivationEmail, ConfirmationEmail
+
+
+def set_saffron_info(context):
+    """
+    デフォルトのdomain, protocolはAPIサーバの実行URLが使用されるため、環境変数でオーバーライドする
+    """
+    # フロントエンドの実行ドメイン及びプロトコル
+    context['domain'] = settings.PETALS_DOMAIN
+    context['protocol'] = settings.PETALS_PROTOCOL
+    # 運営の問い合わせ情報を埋め込み
+    context['management_team_name'] = settings.MANAGEMENT_TEAM_NAME
+    context['management_team_email'] = settings.MANAGEMENT_TEAM_EMAIL
+    return context
 
 
 class SaffronActivationEmail(ActivationEmail, object):
@@ -13,11 +26,18 @@ class SaffronActivationEmail(ActivationEmail, object):
         :return: context
         """
         context = super(SaffronActivationEmail, self).get_context_data()
-        # デフォルトのdomain, protocolはAPIサーバの実行URLが使用される
-        # 環境変数を設定してオーバーライド
-        context['domain'] = settings.PETALS_DOMAIN
-        context['protocol'] = settings.PETALS_PROTOCOL
-        # 運営の問い合わせ情報を埋め込み
-        context['management_team_name'] = settings.MANAGEMENT_TEAM_NAME
-        context['management_team_email'] = settings.MANAGEMENT_TEAM_EMAIL
-        return context
+        return set_saffron_info(context)
+
+
+class SaffronConfirmationEmail(ConfirmationEmail, object):
+    """
+    アカウントアクティベート後の確認メール本文作成クラス
+    """
+    def get_context_data(self, **kwargs):
+        """
+        テンプレートに埋め込む変数をオーバーライド
+        :param kwargs:
+        :return: context
+        """
+        context = super(SaffronConfirmationEmail, self).get_context_data(**kwargs)
+        return set_saffron_info(context)
