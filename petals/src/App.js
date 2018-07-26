d409ac20ad12343b69da3ea2d3d2673e30baeaa2
taxio <taxio.2029@gmail.com>
2018-07-27 01:03:18 +0900

    routing

--- petals/src/App.js
+++ petals/src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {HashRouter, Switch, Route} from 'react-router-dom';
 
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 import Home from './components/Home';
+
+class Base extends Component {
+  render() {
+    return(
+      <div>
+        <NavBar/>
+        {this.props.children}
+        <Footer/>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Route exact path='/' component={Home}/>
-        </div>
-      </BrowserRouter>
+      <HashRouter>
+        <Route path='/' component={MainRoute}/>
+      </HashRouter>
     );
   }
 }
+
+const MainRoute = () => (
+  <Base>
+    <Switch>
+      <Route path='/' component={Home}/>
+    </Switch>
+  </Base>
+);
 
 export default App;
--- petals/src/components/Home.js
+++ petals/src/components/Home.js
@@ -5,23 +5,17 @@ import NavBar from './NavBar';
 import Footer from './Footer';
 
 const Home = () => (
-  <div>
-    <NavBar/>
-
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-6 col-md-7 col-sm-6">
-          <div className="jumbotron">
-            <h1>研究室配属支援</h1>
-            <p>
-              <Link to="#" className="btn btn-primary">Saffronについて</Link>
-            </p>
-          </div>
+  <div className="container">
+    <div className="row">
+      <div className="col-lg-6 col-md-7 col-sm-6">
+        <div className="jumbotron">
+          <h1>研究室配属支援</h1>
+          <p>
+            <Link to="#" className="btn btn-primary">Saffronについて</Link>
+          </p>
         </div>
       </div>
     </div>
-
-    <Footer/>
   </div>
 );
 
