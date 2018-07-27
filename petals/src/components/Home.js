a32339955e10193c70715c4b2e972aa3cee591cb
taxio <taxio.2029@gmail.com>
2018-07-27 16:26:58 +0900

    home

--- petals/src/components/Home.js
+++ petals/src/components/Home.js
@@ -1,21 +1,12 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
-import NavBar from './NavBar';
-import Footer from './Footer';
-
 const Home = () => (
-  <div className="container">
-    <div className="row">
-      <div className="col-lg-6 col-md-7 col-sm-6">
-        <div className="jumbotron">
-          <h1>研究室配属支援</h1>
-          <p>
-            <Link to="#" className="btn btn-primary">Saffronについて</Link>
-          </p>
-        </div>
-      </div>
-    </div>
+  <div className="jumbotron">
+    <h1>研究室配属支援</h1>
+    <p>
+      <Link to="#" className="btn btn-primary">Saffronについて</Link>
+    </p>
   </div>
 );
 
