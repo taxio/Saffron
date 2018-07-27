963d7f05d21e791728728134afb9002b51365034
taxio <taxio.2029@gmail.com>
2018-07-27 16:14:47 +0900

    footer

--- petals/src/components/Footer.js
+++ petals/src/components/Footer.js
@@ -1,12 +1,10 @@
 import React, {Component} from 'react';
 
 const Footer = () => (
-  <footer className="small">
+  <footer className="footer">
     <div className="container">
-      <div className="row">
-        <div className="col-xs-12 text-center copyright">
-          © 2018 StudioAquatan
-        </div>
+      <div className="col-xs-12 text-center copyright">
+        © 2018 StudioAquatan
       </div>
     </div>
   </footer>
