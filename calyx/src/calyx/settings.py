9551ee18b4788454ecddf6710d9793c596ee69dd
Pudding <s.kokuryo@gmail.com>
2018-10-11 14:00:36 +0900

    Update model definitions
    - Fix bug about createsuperuser command
    - Remove year app and migrate into course app

--- calyx/src/calyx/settings.py
+++ calyx/src/calyx/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     'rest_framework_jwt',
     'rest_framework_swagger',
     'djoser',
-    'years',
     'courses',
     'users',
     'labs'
--- calyx/src/years/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class YearsConfig(AppConfig):
-    name = 'years'
