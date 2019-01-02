b8eec35a136245943415ebb1006eda5648182fbc
Pudding <s.kokuryo@gmail.com>
2019-01-02 23:35:06 +0900

    Add url routing

--- calyx/src/courses/urls.py
+++ calyx/src/courses/urls.py
@@ -1,6 +1,12 @@
+from django.urls import path, include
 from rest_framework import routers
 from .views import CourseViewSet
 
 router = routers.DefaultRouter()
 
-router.register('course', CourseViewSet, basename='course')
+router.register('', CourseViewSet, basename='course')
+
+app_name = 'course'
+urlpatterns = [
+    path('', include(router.urls))
+]
--- calyx/src/courses/views.py
+++ calyx/src/courses/views.py
@@ -6,6 +6,15 @@ from .serializers import CourseSerializer
 class CourseViewSet(viewsets.ModelViewSet):
     """
     課程のAPIビュー
+
+    retrieve:
+        課程の詳細情報と所属するユーザを取得する
+    list:
+        課程の一覧を取得する
+    create:
+        新しい課程を作成する
+    destroy:
+        指定した課程を削除する
     """
 
     queryset = Course.objects.prefetch_related('users').select_related('year').all()
