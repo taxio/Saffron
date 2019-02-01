a815dcdf504049addc2441667fd724fc5f5d8192
taxio <taxio.2029@gmail.com>
2019-02-01 15:18:53 +0900

    fix Signup Component

--- petals/src/components/Signup.tsx
+++ petals/src/components/Signup.tsx
@@ -10,27 +10,25 @@ import {
   FormControlLabel,
   FormHelperText,
   Grid,
-  Input,
-  InputLabel,
+  TextField,
 } from '@material-ui/core';
 import * as React from 'react';
 import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 
-import { PasswordValidationError, validatePassword, validateUsername } from '../api/auth';
+import { Field, InjectedFormProps, reduxForm, SubmissionError, WrappedFieldProps } from 'redux-form';
 import { createUser } from '../api/users';
+import { validatePasswordWithErrMsg, validateUsernameWithErrMsg } from '../lib/validations';
 
-interface SignupProps extends RouteComponentProps {}
+interface FormParams {
+  username: string;
+  password: string;
+  confirmPassword: string;
+  agreeWithTermsOfService: any;
+}
+
+interface SignupProps extends RouteComponentProps, InjectedFormProps {}
 
 interface SignupState {
-  username: string;
-  usernameErr: boolean;
-  password: string;
-  passwordErrMsg: string;
-  confirmPassword: string;
-  confirmPasswordErrMsg: string;
-  agreedWithTermsOfService: boolean;
-  agreedWithTermsOfServiceErr: boolean;
-  signupErrMsg: string;
   showDialog: boolean;
 }
 
@@ -38,165 +36,80 @@ class Signup extends React.Component<SignupProps, SignupState> {
   constructor(props: SignupProps) {
     super(props);
     this.state = {
-      username: '',
-      usernameErr: false,
-      password: '',
-      passwordErrMsg: '',
-      confirmPassword: '',
-      confirmPasswordErrMsg: '',
-      agreedWithTermsOfService: false,
-      agreedWithTermsOfServiceErr: false,
-      signupErrMsg: '',
       showDialog: false,
     };
-
-    this.handleChangeUsername = this.handleChangeUsername.bind(this);
-    this.handleChangePassword = this.handleChangePassword.bind(this);
-    this.handleChangeConfirmPassword = this.handleChangeConfirmPassword.bind(this);
-    this.handleSignup = this.handleSignup.bind(this);
-    this.handleCloseDialog = this.handleCloseDialog.bind(this);
   }
 
-  public handleChangeUsername(e: React.ChangeEvent<HTMLInputElement>) {
-    const username = e.target.value;
-    const usernameErr = !validateUsername(username);
-    this.setState({ username, usernameErr });
-  }
+  public renderTextField = (props: WrappedFieldProps & { label: string; type: string }) => (
+    <FormControl fullWidth={true} error={Boolean(props.meta.error)} style={{ padding: '10px 0px' }}>
+      <TextField label={props.label} margin="normal" type={props.type} {...props.input} />
+      {props.meta.error ? <FormHelperText>{props.meta.error}</FormHelperText> : null}
+    </FormControl>
+  );
 
-  public handleChangePassword(e: React.ChangeEvent<HTMLInputElement>) {
-    const password = e.target.value;
-    let passwordErrMsg = '';
+  public renderCheckboxField = (props: WrappedFieldProps & { label: string }) => (
+    <FormControl fullWidth={true} error={Boolean(props.meta.error)} style={{ padding: '10px 0px' }}>
+      <FormControlLabel
+        control={<Checkbox value={props.label} checked={props.input.checked} onChange={props.input.onChange} />}
+        label={
+          <React.Fragment>
+            <Link to="/termsofservice">利用規約</Link>に同意する
+          </React.Fragment>
+        }
+      />
+      {props.meta.error ? <FormHelperText id="signup-agreed-error-text">{props.meta.error}</FormHelperText> : null}
+    </FormControl>
+  );
 
-    const ret = validatePassword(password);
-    switch (ret) {
-      case PasswordValidationError.NONE:
-        break;
-      case PasswordValidationError.LENGTH:
-        passwordErrMsg = 'パスワードは8文字以上にしてください';
-        break;
-      case PasswordValidationError.UNAVAILABLE:
-        passwordErrMsg = '使用不可能な文字が含まれています';
-        break;
+  public handleSubmit = (values: FormParams) => {
+    console.log(values);
+    const usernameErrMsg = validateUsernameWithErrMsg(values.username);
+    const passwordErrMsg = validatePasswordWithErrMsg(values.password);
+    const confirmPasswordErrMsg = values.password === values.confirmPassword ? '' : 'パスワードが一致しません';
+    const agreeWithTermsOfServiceErrMsg = values.agreeWithTermsOfService ? '' : '利用規約への同意は必須です';
+
+    if (usernameErrMsg || passwordErrMsg || confirmPasswordErrMsg) {
+      throw new SubmissionError({
+        username: usernameErrMsg,
+        password: passwordErrMsg,
+        confirmPassword: confirmPasswordErrMsg,
+        agreeWithTermsOfService: agreeWithTermsOfServiceErrMsg,
+        _error: '入力項目に誤りがあります',
+      });
     }
 
-    this.setState({ password, passwordErrMsg });
-  }
-
-  public handleChangeConfirmPassword(e: React.ChangeEvent<HTMLInputElement>) {
-    const confirmPassword = e.target.value;
-    let confirmPasswordErrMsg = '';
-
-    if (confirmPassword !== this.state.password) {
-      confirmPasswordErrMsg = 'パスワードが一致しません';
-    }
-
-    this.setState({ confirmPassword, confirmPasswordErrMsg });
-  }
-
-  public handleSignup() {
-    if (
-      this.state.usernameErr ||
-      this.state.confirmPasswordErrMsg ||
-      !this.state.username ||
-      !this.state.password ||
-      !this.state.confirmPassword
-    ) {
-      this.setState({ signupErrMsg: '入力情報に誤りがあります' });
-      return;
-    }
-    if (!this.state.agreedWithTermsOfService) {
-      this.setState({ agreedWithTermsOfServiceErr: true });
-      return;
-    }
-
-    createUser(this.state.username, this.state.password).then(success => {
+    return createUser(values.username, values.password).then(success => {
       if (!success) {
-        this.setState({ signupErrMsg: 'アカウント作成に失敗しました' });
-        return;
+        throw new SubmissionError({ _error: 'アカウント作成に失敗しました' });
       }
       this.setState({ showDialog: true });
     });
-  }
+  };
 
   public handleCloseDialog() {
     this.props.history.push('/');
   }
 
   public render(): React.ReactNode {
-    const formControlStyle = { padding: '10px 0px' };
+    const { error, handleSubmit } = this.props;
 
     return (
       <Grid container={true} justify="center">
         <Grid item={true} xs={10} sm={8} md={7} lg={6} xl={5}>
           <Card style={{ marginTop: 30, padding: 20 }}>
             <CardContent style={{ textAlign: 'center' }}>
-              <form>
-                <FormControl fullWidth={true} style={formControlStyle} error={this.state.usernameErr}>
-                  <InputLabel htmlFor="signup-email">ユーザー名</InputLabel>
-                  <Input id="signup-email" value={this.state.username} onChange={this.handleChangeUsername} />
-                  <FormHelperText id="signup-email-text">
-                    bまたはm,dで始まる8文字の大学ユーザー名を入力してください
-                  </FormHelperText>
-                </FormControl>
+              <form onSubmit={handleSubmit(this.handleSubmit)} autoComplete="off">
+                <Field name="username" label="大学ユーザー名" type="text" component={this.renderTextField} />
+                <Field name="password" label="パスワード" type="password" component={this.renderTextField} />
+                <Field
+                  name="confirmPassword"
+                  label="パスワード再入力"
+                  type="password"
+                  component={this.renderTextField}
+                />
+                <Field name="agreeWithTermsOfService" label="利用規約への同意" component={this.renderCheckboxField} />
 
-                <FormControl fullWidth={true} error={Boolean(this.state.passwordErrMsg)} style={formControlStyle}>
-                  <InputLabel htmlFor="signup-password">パスワード</InputLabel>
-                  <Input
-                    id="signup-password"
-                    type="password"
-                    value={this.state.password}
-                    onChange={this.handleChangePassword}
-                    autoComplete="off"
-                  />
-                  {this.state.passwordErrMsg ? (
-                    <FormHelperText id="signup-confirm-password-error-text">{this.state.passwordErrMsg}</FormHelperText>
-                  ) : null}
-                </FormControl>
-
-                <FormControl
-                  fullWidth={true}
-                  error={Boolean(this.state.confirmPasswordErrMsg)}
-                  style={formControlStyle}
-                >
-                  <InputLabel htmlFor="signup-confirm-password">パスワード再入力</InputLabel>
-                  <Input
-                    id="signup-confirm-password"
-                    type="password"
-                    value={this.state.confirmPassword}
-                    onChange={this.handleChangeConfirmPassword}
-                    autoComplete="off"
-                  />
-                  {this.state.confirmPasswordErrMsg ? (
-                    <FormHelperText id="signup-confirm-password-error-text">
-                      {this.state.confirmPasswordErrMsg}
-                    </FormHelperText>
-                  ) : null}
-                </FormControl>
-
-                <FormControl fullWidth={true} error={this.state.agreedWithTermsOfServiceErr} style={formControlStyle}>
-                  <FormControlLabel
-                    control={
-                      <Checkbox
-                        checked={this.state.agreedWithTermsOfService}
-                        onChange={e =>
-                          this.setState({ agreedWithTermsOfService: !this.state.agreedWithTermsOfService })
-                        }
-                        value="AgreedWithTermsOfService"
-                      />
-                    }
-                    label={
-                      <React.Fragment>
-                        <Link to="/termsofservice">利用規約</Link>に同意する
-                      </React.Fragment>
-                    }
-                  />
-                  {this.state.agreedWithTermsOfServiceErr ? (
-                    <FormHelperText id="signup-agreed-error-text">利用規約に同意してください</FormHelperText>
-                  ) : null}
-                </FormControl>
-
-                <FormControl fullWidth={true} style={formControlStyle} error={Boolean(this.state.signupErrMsg)}>
-                  {this.state.signupErrMsg ? <FormHelperText>{this.state.signupErrMsg}</FormHelperText> : null}
+                <FormControl fullWidth={true} style={{ padding: '10px 0px' }} error={Boolean(error)}>
                   <Button
                     style={{
                       marginTop: 16,
@@ -205,10 +118,11 @@ class Signup extends React.Component<SignupProps, SignupState> {
                     }}
                     variant="contained"
                     color="primary"
-                    onClick={this.handleSignup}
+                    type="submit"
                   >
                     Sign up
                   </Button>
+                  {error ? <FormHelperText>{error}</FormHelperText> : null}
                 </FormControl>
               </form>
               <Dialog fullWidth={true} maxWidth="xs" open={this.state.showDialog} onClose={this.handleCloseDialog}>
@@ -225,4 +139,6 @@ class Signup extends React.Component<SignupProps, SignupState> {
   }
 }
 
-export default withRouter(Signup);
+export default reduxForm({
+  form: 'signupForm',
+})(withRouter(Signup));
