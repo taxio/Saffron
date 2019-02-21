d2598b1b138506a849519b6ff764b504e19afd09
Pudding <s.kokuryo@gmail.com>
2019-02-22 00:26:41 +0900

    Remove django-rest-swagger

--- calyx/src/calyx/settings.py
+++ calyx/src/calyx/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework.authtoken',
-    'rest_framework_swagger',
     'corsheaders',
     'djoser',
     'courses',
--- calyx/src/calyx/urls.py
+++ calyx/src/calyx/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_swagger.views import get_swagger_view
-from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -37,8 +35,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include(jwt_urls)),
-    path('swagger/', get_swagger_view(title='Calyx API Document')),
-    path('yasg/', schema_view.with_ui('swagger', cache_timeout=0)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
     path('', include(accounts_urls)),
     path('', include(course_urls)),
 ]
