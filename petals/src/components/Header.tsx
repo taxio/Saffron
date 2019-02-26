013d49a63301d03dbdd6380a42600f982e62c203
taxio <taxio.2029@gmail.com>
2019-02-26 21:22:26 +0900

    fix Header Design

--- petals/src/components/Header.tsx
+++ petals/src/components/Header.tsx
@@ -8,7 +8,10 @@ import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import AccountCircle from '@material-ui/icons/AccountCircle';
+import MoreHoriz from '@material-ui/icons/MoreHoriz';
+
+import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles';
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
 
 import * as React from 'react';
 import { connect } from 'react-redux';
@@ -16,17 +19,60 @@ import { RouteComponentProps, withRouter } from 'react-router-dom';
 import { Dispatch } from 'redux';
 
 import { AuthAction, setLoginState } from '../actions/auth';
+import { UserAction, userFetchRequest } from '../actions/user';
 import { logout } from '../lib/auth';
+import * as model from '../model';
 import { PetalsStore } from '../store';
 
-interface HeaderProps extends RouteComponentProps<any> {
+const headerStyles = (theme: Theme) => {
+  return createStyles({
+    grow: {
+      flexGrow: 1,
+    },
+    sectionDesktop: {
+      display: 'none',
+      [theme.breakpoints.up('md')]: {
+        display: 'flex',
+      },
+    },
+    sectionMobile: {
+      display: 'flex',
+      [theme.breakpoints.up('md')]: {
+        display: 'none',
+      },
+    },
+    menuButton: {
+      margin: '0 8px',
+    },
+  });
+};
+
+interface HeaderProps extends RouteComponentProps<any>, WithStyles<typeof headerStyles> {
   isLogin: boolean;
   setLoginState: (isLogin: boolean) => void;
+  fetchUserInfo: () => void;
+  user: model.User | null;
+  isFetching: boolean;
+  fetchError: Error | null;
 }
 
 const Header: React.FC<HeaderProps> = props => {
   const [showDialog, setShowDialog] = React.useState<boolean>(false);
   const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
+
+  React.useEffect(() => {
+    if (props.isLogin) {
+      props.fetchUserInfo();
+    }
+  }, [props.isLogin]);
+
+  const getCourses = (): model.Course[] => {
+    if (!props.user) {
+      return [];
+    }
+
+    return props.user.courses;
+  };
 
   const handleOpenDialog = () => {
     setShowDialog(true);
@@ -45,28 +91,68 @@ const Header: React.FC<HeaderProps> = props => {
     setAnchorEl(null);
   };
 
-  const handleMenu = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+  const handleOpenMenu = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
     setAnchorEl(e.currentTarget);
   };
 
-  const handleClickLogin = () => {
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
+  const toLogin = () => {
     props.history.push('/login');
     setAnchorEl(null);
   };
 
-  const handleClickSignup = () => {
+  const toSignUp = () => {
     props.history.push('/signup');
     setAnchorEl(null);
   };
 
-  const handleClose = () => {
+  const toSettings = () => {
+    props.history.push('/settings');
     setAnchorEl(null);
   };
 
-  const handleToProfile = () => {
-    props.history.push('/profile');
+  const toCourse = (coursePk: number) => {
+    props.history.push(`/courses/${coursePk}`);
     setAnchorEl(null);
   };
+
+  const renderMobileMenu = () => (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={Boolean(anchorEl)}
+      onClose={handleCloseMenu}
+    >
+      <MenuItem key={0} onClick={toLogin}>
+        ログイン
+      </MenuItem>
+      <MenuItem key={1} onClick={toSignUp}>
+        新規登録
+      </MenuItem>
+    </Menu>
+  );
+
+  const renderAuthedMobileMenu = () => (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={Boolean(anchorEl)}
+      onClose={handleCloseMenu}
+    >
+      {getCourses().map((course: model.Course, idx: number) => (
+        <MenuItem key={idx} onClick={() => toCourse(course.pk)}>
+          {course.name}
+        </MenuItem>
+      ))}
+      <MenuItem onClick={toSettings}>設定</MenuItem>
+      <MenuItem onClick={handleLogout}>ログアウト</MenuItem>
+    </Menu>
+  );
 
   return (
     <AppBar position="static" style={{ boxShadow: 'none' }}>
@@ -80,63 +166,45 @@ const Header: React.FC<HeaderProps> = props => {
           Saffron
         </Typography>
 
-        {props.isLogin ? (
-          <React.Fragment>
-            <IconButton
-              aria-owns={open ? 'menu-appbar' : undefined}
-              aria-haspopup="true"
-              onClick={handleMenu}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleToProfile}>設定</MenuItem>
-              <MenuItem onClick={handleOpenDialog}>ログアウト</MenuItem>
-            </Menu>
-          </React.Fragment>
-        ) : (
-          <React.Fragment>
-            <IconButton
-              aria-owns={open ? 'menu-appbar' : undefined}
-              aria-haspopup="true"
-              onClick={handleMenu}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleClickLogin}>ログイン</MenuItem>
-              <MenuItem onClick={handleClickSignup}>新規登録</MenuItem>
-            </Menu>
-          </React.Fragment>
-        )}
+        <div className={props.classes.grow} />
+        <div className={props.classes.sectionDesktop}>
+          {props.isLogin ? (
+            <React.Fragment>
+              {getCourses().map((course: model.Course, idx: number) => (
+                <Button
+                  key={idx}
+                  color="inherit"
+                  className={props.classes.menuButton}
+                  onClick={() => toCourse(course.pk)}
+                >
+                  {course.name}
+                </Button>
+              ))}
+              <Button color="inherit" className={props.classes.menuButton} onClick={toSettings}>
+                設定
+              </Button>
+              <Button color="inherit" className={props.classes.menuButton} onClick={handleOpenDialog}>
+                ログアウト
+              </Button>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              <Button color="inherit" className={props.classes.menuButton} onClick={toLogin}>
+                ログイン
+              </Button>
+              <Button color="inherit" className={props.classes.menuButton} onClick={toSignUp}>
+                新規登録
+              </Button>
+            </React.Fragment>
+          )}
+        </div>
+        <div className={props.classes.sectionMobile}>
+          <IconButton aria-haspopup="true" onClick={handleOpenMenu} color="inherit">
+            <MoreHoriz />
+          </IconButton>
+        </div>
+        {props.isLogin ? renderAuthedMobileMenu() : renderMobileMenu()}
+
         <Dialog fullWidth={true} maxWidth="xs" open={showDialog} onClose={handleCloseDialog}>
           <DialogTitle>ログアウトしてもよろしいですか？</DialogTitle>
           <DialogActions>
@@ -153,29 +221,41 @@ const Header: React.FC<HeaderProps> = props => {
 
 interface StateFromProps {
   isLogin: boolean;
+  user: model.User | null;
+  isFetching: boolean;
+  fetchError: Error | null;
 }
 
 interface DispatchFromProps {
   setLoginState: (isLogin: boolean) => void;
+  fetchUserInfo: () => void;
 }
 
 function mapStateToProps(state: PetalsStore): StateFromProps {
   return {
     isLogin: state.auth.isLogin,
+    user: state.user.user,
+    isFetching: state.user.isFetching,
+    fetchError: state.user.error,
   };
 }
 
-function mapDispatchToProps(dispatch: Dispatch<AuthAction>): DispatchFromProps {
+function mapDispatchToProps(dispatch: Dispatch<AuthAction | UserAction>): DispatchFromProps {
   return {
     setLoginState: (isLogin: boolean) => {
       dispatch(setLoginState(isLogin));
     },
+    fetchUserInfo: () => {
+      dispatch(userFetchRequest());
+    },
   };
 }
 
-export default withRouter(
-  connect<StateFromProps, DispatchFromProps, {}>(
-    mapStateToProps,
-    mapDispatchToProps
-  )(Header)
+export default withStyles(headerStyles)(
+  withRouter(
+    connect<StateFromProps, DispatchFromProps, {}>(
+      mapStateToProps,
+      mapDispatchToProps
+    )(Header)
+  )
 );
