7d6d1b284d144c36dcc2ef2a8aec621266a9819e
taxio <taxio.2029@gmail.com>
2019-02-28 14:17:00 +0900

    add style params to GridPaper

--- petals/src/components/Common/GridPaper.tsx
+++ petals/src/components/Common/GridPaper.tsx
@@ -2,10 +2,10 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import * as React from 'react';
 
-const GridPaper: React.FC = props => (
+const GridPaper: React.FC<{ style?: any }> = props => (
   <Grid container={true} justify="center">
     <Grid item={true} xs={12} sm={8} md={7} lg={6} xl={5}>
-      <Paper style={{ marginTop: 20, padding: 30 }}>{props.children}</Paper>
+      <Paper style={{ ...props.style, marginTop: 20, padding: 30 }}>{props.children}</Paper>
     </Grid>
   </Grid>
 );
