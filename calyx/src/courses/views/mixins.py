2041667e84c52803d797f4cbd7985f728c98fc2c
Pudding <s.kokuryo@gmail.com>
2019-03-01 00:52:15 +0900

    Implement CourseNestedMixin

--- calyx/src/courses/views/mixins.py
+++ calyx/src/courses/views/mixins.py
@@ -1,3 +1,7 @@
+from rest_framework import exceptions
+from courses.models import Course
+
+
 class NestedViewSetMixin(object):
 
     def get_queryset(self):
@@ -9,3 +13,19 @@ class NestedViewSetMixin(object):
                 orm_filters[field_name] = self.kwargs[query_param]
             return queryset.filter(**orm_filters)
         return queryset
+
+
+class CourseNestedMixin(NestedViewSetMixin):
+
+    def get_course(self):
+        course_pk = self.kwargs.get("course_pk", None)
+        if course_pk is None:
+            return None
+        if isinstance(course_pk, str):
+            course_pk = int(course_pk)
+        try:
+            course = Course.objects.get(pk=course_pk)
+        except Course.DoesNotExist:
+            return exceptions.NotFound("指定された課程は存在しません")
+        self.check_object_permission(self.request, course)
+        return course
