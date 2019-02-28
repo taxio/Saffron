1cde4f76c33e760bcbca41f399207967d1f166d8
taxio <taxio.2029@gmail.com>
2019-02-28 13:13:43 +0900

    add common component, GridPaper

---
+++ petals/src/components/Common/GridPaper.tsx
@@ -0,0 +1,13 @@
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import * as React from 'react';
+
+const GridPaper: React.FC = props => (
+  <Grid container={true} justify="center">
+    <Grid item={true} xs={12} sm={8} md={7} lg={6} xl={5}>
+      <Paper style={{ marginTop: 20, padding: 30 }}>{props.children}</Paper>
+    </Grid>
+  </Grid>
+);
+
+export default GridPaper;
