04d5daf616e1000a0ce39bec4c6e66e71be79ab2
taxio <taxio.2029@gmail.com>
2019-02-28 20:18:44 +0900

    use GridPaper

--- petals/src/components/Auth/ChangePassword.tsx
+++ petals/src/components/Auth/ChangePassword.tsx
@@ -1,18 +1,16 @@
 import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import FormControl from '@material-ui/core/FormControl';
 import FormHelperText from '@material-ui/core/FormHelperText';
-import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import * as React from 'react';
 import { RouteComponentProps, withRouter } from 'react-router';
 import { Field, InjectedFormProps, reduxForm, SubmissionError, WrappedFieldProps } from 'redux-form';
 import * as passwordApi from '../../api/password';
 import { validatePasswordWithErrMsg } from '../../lib/validations';
+import GridPaper from '../Common/GridPaper';
 
 interface FormParams {
   currentPassword: string;
@@ -72,49 +70,43 @@ class ChangePassword extends React.Component<ChangePasswordProps, ChangePassword
     const { error, handleSubmit } = this.props;
 
     return (
-      <Grid container={true} justify="center">
-        <Grid item={true} xs={10} sm={8} md={7} lg={6} xl={5}>
-          <Card style={{ marginTop: 30, padding: 20 }}>
-            <CardContent style={{ textAlign: 'center' }}>
-              <form onSubmit={handleSubmit(this.handleSubmit)} autoComplete="off">
-                <Field name="currentPassword" label="現在のパスワード" type="password" component={this.renderField} />
+      <GridPaper>
+        <form onSubmit={handleSubmit(this.handleSubmit)} autoComplete="off">
+          <Field name="currentPassword" label="現在のパスワード" type="password" component={this.renderField} />
 
-                <Field name="newPassword" label="新しいパスワード" type="password" component={this.renderField} />
+          <Field name="newPassword" label="新しいパスワード" type="password" component={this.renderField} />
 
-                <Field
-                  name="confirmNewPassword"
-                  label="新しいパスワードを再入力"
-                  type="password"
-                  component={this.renderField}
-                />
+          <Field
+            name="confirmNewPassword"
+            label="新しいパスワードを再入力"
+            type="password"
+            component={this.renderField}
+          />
 
-                <FormControl fullWidth={true} style={{ padding: '10px 0px' }} error={Boolean(error)}>
-                  <Button
-                    type="submit"
-                    color="primary"
-                    variant="contained"
-                    style={{
-                      marginTop: 16,
-                      marginBottom: 8,
-                      boxShadow: 'none',
-                    }}
-                  >
-                    パスワード変更
-                  </Button>
-                  {error ? <FormHelperText>{error}</FormHelperText> : null}
-                </FormControl>
-              </form>
+          <FormControl fullWidth={true} style={{ padding: '10px 0px' }} error={Boolean(error)}>
+            <Button
+              type="submit"
+              color="primary"
+              variant="contained"
+              style={{
+                marginTop: 16,
+                marginBottom: 8,
+                boxShadow: 'none',
+              }}
+            >
+              パスワード変更
+            </Button>
+            {error ? <FormHelperText>{error}</FormHelperText> : null}
+          </FormControl>
+        </form>
 
-              <Dialog fullWidth={true} maxWidth="xs" open={this.state.showDialog} onClose={this.handleCloseDialog}>
-                <DialogTitle>パスワードを変更しました</DialogTitle>
-                <DialogActions>
-                  <Button onClick={this.handleCloseDialog}>閉じる</Button>
-                </DialogActions>
-              </Dialog>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
+        <Dialog fullWidth={true} maxWidth="xs" open={this.state.showDialog} onClose={this.handleCloseDialog}>
+          <DialogTitle>パスワードを変更しました</DialogTitle>
+          <DialogActions>
+            <Button onClick={this.handleCloseDialog}>閉じる</Button>
+          </DialogActions>
+        </Dialog>
+      </GridPaper>
     );
   }
 }
