858865437b8f8ac444f295db017d66a442f6ccc2
lanqiu <ksumebosi@gmail.com>
2019-03-01 16:58:26 +0900

    Add Post
    not running

--- petals/src/components/Course/HopeLabs.tsx
+++ petals/src/components/Course/HopeLabs.tsx
@@ -12,14 +12,23 @@ import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 // import { connect } from 'react-redux';
 // import { RouteComponentProps, withRouter } from 'react-router-dom';
-import { Field, InjectedFormProps, reduxForm, WrappedFieldProps } from 'redux-form';
+import { Field, InjectedFormProps, reduxForm, SubmissionError, WrappedFieldProps } from 'redux-form';
+import * as AppErr from '../../api/AppErrors';
 import { getLabs } from '../../api/courses';
+import * as courseApi from '../../api/courses';
 import { Lab } from '../../model';
 // import { Dispatch } from 'redux';
 // import { AuthAction, setLoginState } from '../../actions/auth';
 // import * as AppErr from '../../api/AppErrors';
 // import * as auth from '../../lib/auth';
 // import { PetalsStore } from '../../store';
+
+interface FormParams {
+  gpa: number;
+  lab1: number;
+  lab2: number;
+  lab3: number;
+}
 
 interface HopeLabsProps extends RouteComponentProps<any>, InjectedFormProps {}
 
@@ -41,6 +50,65 @@ class HopeLabs extends React.Component<HopeLabsProps, HopeLabsState> {
       this.setState({ labs });
     });
   }
+
+  public handleHopeLabs = (values: FormParams) => {
+    let errMsg = '';
+    let gpaErrMsg = '';
+    let lab1ErrMsg = '';
+    let lab2ErrMsg = '';
+    let lab3ErrMsg = '';
+    if (!values.gpa) {
+      gpaErrMsg = 'GPAを入力してください';
+      errMsg = '未入力項目があります';
+    }
+    if (!values.lab1) {
+      lab1ErrMsg = '第一希望研究室を選択してください';
+      errMsg = '未選択項目があります';
+    }
+    if (!values.lab2) {
+      lab2ErrMsg = '第二希望研究室を選択してください';
+      errMsg = '未選択項目があります';
+    }
+    if (!values.lab3) {
+      lab3ErrMsg = '第三希望研究室を選択してください';
+      errMsg = '未選択項目があります';
+    }
+    if (gpaErrMsg || lab1ErrMsg || lab2ErrMsg || lab3ErrMsg) {
+      throw new SubmissionError({
+        gpa: gpaErrMsg,
+        lab1: lab1ErrMsg,
+        lab2: lab2ErrMsg,
+        lab3: lab3ErrMsg,
+        _error: errMsg,
+      });
+    }
+
+    return courseApi
+      .postRanks(this.props.match.params.coursePk, [values.lab1, values.lab2, values.lab3])
+      .then(courseRes => {
+        const promises: Array<Promise<any>> = [];
+        values.labs.map(lab => {
+          promises.push(courseApi.postLab(courseRes.pk, lab.name, lab.capacity));
+        });
+        Promise.all(promises).then(() => {
+          this.props.history.push('/');
+        });
+      })
+      .catch((e: Error) => {
+        switch (e.constructor) {
+          case AppErr.UnAuthorizedError:
+            this.props.history.push('/login/');
+            throw new SubmissionError({ _error: 'ログインセッションが切れました' });
+          case AppErr.BadRequestError:
+            // TODO: レスポンス中のエラーの表示
+            throw new SubmissionError({ _error: '研究室希望提出に失敗しました' });
+          case AppErr.UnhandledError:
+            throw new SubmissionError({ _error: '研究室希望提出に失敗しました' });
+          default:
+            throw new SubmissionError({ _error: '未知のエラーです' });
+        }
+      });
+  };
 
   public renderGpaField = (props: WrappedFieldProps & { label: string; type: string; style: any }) => (
     <FormControl fullWidth={true} error={Boolean(props.meta.error)} style={{ margin: '5%', width: '150px' }}>
@@ -74,6 +142,8 @@ class HopeLabs extends React.Component<HopeLabsProps, HopeLabsState> {
   );
 
   public render(): React.ReactNode {
+    const { error, handleSubmit } = this.props;
+
     return (
       <Grid container={true} justify="center">
         <Grid item={true} xs={10} sm={8} md={7} lg={6} xl={5}>
@@ -82,20 +152,23 @@ class HopeLabs extends React.Component<HopeLabsProps, HopeLabsState> {
               <Typography component="h1" variant="h5" align="center">
                 研究室希望提出
               </Typography>
-              <form autoComplete="off">
+              <form onSubmit={handleSubmit(this.handleHopeLabs)} autoComplete="off">
                 <Field name="gpa" label="GPA" type="number" component={this.renderGpaField} />
 
                 <Field name="labsName1" label="第一希望" component={this.renderLabSelectField} />
                 <Field name="labsName2" label="第二希望" component={this.renderLabSelectField} />
                 <Field name="labsName3" label="第三希望" component={this.renderLabSelectField} />
-                <Button
-                  type="submit"
-                  color="primary"
-                  variant="contained"
-                  style={{ margin: '5%', marginTop: '15%', width: '60%', boxShadow: 'none' }}
-                >
-                  Submit
-                </Button>
+                <FormControl fullWidth={true} error={Boolean(error)}>
+                  <Button
+                    type="submit"
+                    color="primary"
+                    variant="contained"
+                    style={{ margin: '5%', marginTop: '15%', width: '60%', boxShadow: 'none' }}
+                  >
+                    Submit
+                  </Button>
+                  {error ? <FormHelperText>{error}</FormHelperText> : null}
+                </FormControl>
               </form>
             </CardContent>
           </Card>
@@ -105,32 +178,6 @@ class HopeLabs extends React.Component<HopeLabsProps, HopeLabsState> {
   }
 }
 
-// interface StateFromProps {
-//   isLogin: boolean;
-// }
-//
-// interface DispatchFromProps {
-//   setLoginState: (isLogin: boolean) => void;
-// }
-//
-// function mapStateToProps(state: PetalsStore): StateFromProps {
-//   return {
-//     isLogin: ,
-//   };
-// }
-//
-// function mapDispatchToProps(dispatch: Dispatch<AuthAction>): DispatchFromProps {
-//   return {
-//     setLoginState: (isLogin: boolean) => {
-//       dispatch(setLoginState(isLogin));
-//     },
-//   };
-// }
-
 export default reduxForm({
   form: 'hopeLabsForm',
 })(HopeLabs);
-// connect( state => ({
-//   labName: selector(state, 'labName'),
-// }))(withRouter(HopeLabs))
-// );
