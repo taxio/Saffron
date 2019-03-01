9919839e07189783c78712e5459aa51b44308134
taxio <taxio.2029@gmail.com>
2019-03-01 20:09:16 +0900

    api index

---
+++ petals/src/api/index.ts
@@ -0,0 +1,8 @@
+import * as errors from './AppErrors';
+import * as courses from './courses';
+import * as me from './me';
+import * as password from './password';
+import * as users from './users';
+import * as years from './years';
+
+export { errors, courses, me, password, users, years };
