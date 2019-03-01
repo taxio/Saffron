9bb6d2da22e08fe859eae5b045b5ef2fd1ca2443
taxio <taxio.2029@gmail.com>
2019-03-01 16:41:31 +0900

    display by year

--- petals/src/components/Course/CourseList.tsx
+++ petals/src/components/Course/CourseList.tsx
@@ -1,4 +1,6 @@
-import { List, ListItem, Typography } from '@material-ui/core';
+import { Button, Divider, List, ListItem, ListItemIcon, ListItemText, Typography } from '@material-ui/core';
+import BarChart from '@material-ui/icons/BarChart';
+import Person from '@material-ui/icons/Person';
 
 import * as React from 'react';
 import { connect } from 'react-redux';
@@ -11,20 +13,20 @@ import * as model from '../../model';
 import { PetalsStore } from '../../store';
 import GridPaper from '../Common/GridPaper';
 
-import * as courseApi from '../../api/courses';
+import * as yearApi from '../../api/years';
 
 interface CourseListProps extends RouteComponentProps {
   fetchUserInfo: () => void;
 }
 
 const CourseList: React.FC<CourseListProps> = props => {
-  const [courses, setCourses] = React.useState<model.Course[]>([]);
+  const [years, setYears] = React.useState<model.Year[]>([]);
 
   React.useEffect(() => {
-    courseApi
-      .getCourses()
-      .then((res: model.Course[]) => {
-        setCourses(res);
+    yearApi
+      .getYears()
+      .then((res: model.Year[]) => {
+        setYears(res);
       })
       .catch((e: Error) => {
         console.log(e);
@@ -39,13 +41,34 @@ const CourseList: React.FC<CourseListProps> = props => {
 
       {/* TODO: 検索機能 */}
 
-      <List>
-        {courses.map((course: model.Course, idx: number) => (
-          <ListItem key={idx} button={true} onClick={() => props.history.push(`/courses/${course.pk}`)}>
-            {`${course.year}年度　 ${course.name}`}
-          </ListItem>
-        ))}
-      </List>
+      {years.map((year: model.Year, yearIdx: number) => (
+        <div key={yearIdx} style={{ marginTop: 20 }}>
+          <Typography variant="h5">{year.year}年度</Typography>
+          <List>
+            {year.courses.map((course: model.Course, idx: number) => (
+              <ListItem key={idx} button={true} onClick={() => props.history.push(`/courses/${course.pk}`)}>
+                <ListItemIcon>
+                  <Person color={course.config.show_username ? 'primary' : 'disabled'} />
+                </ListItemIcon>
+                <ListItemIcon>
+                  <BarChart color={course.config.show_gpa ? 'primary' : 'disabled'} />
+                </ListItemIcon>
+                <ListItemText primary={course.name} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+        </div>
+      ))}
+
+      <Button
+        variant="contained"
+        color="primary"
+        style={{ width: '100%', marginTop: 30 }}
+        onClick={() => props.history.push('/courses/create')}
+      >
+        課程新規作成
+      </Button>
     </GridPaper>
   );
 };
